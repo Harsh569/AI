bb90f42e8a9935d5f2786288c5a0560d434bbf93
Harshad Sonawane <55707077+Harsh569@users.noreply.github.com>
2025-10-04 11:18:21 +0530

    Create Roadmap.sh

---
+++ GenAI/Roadmap.sh
@@ -0,0 +1,77 @@
+#####################Shubham Thakrey###############################
+1) Basics of NLP
+2) LLM Architecture
+3) Types of LLM
+4) Framework used
+5) Vector db
+6) RAG
+7) Multimodal
+8) APIS
+9) OpenSource Local Model
+10) Prompt Engineering
+11) Types of Model, (Quantise, Model prunning and distillation)
+
+
+################Course####################################
+
+
+Introduction to LLM and Generative AI
+AI Agents and Agenetic Workflows
+Building Basic Chat Applications
+Chat Over Large Documents
+Retrieval-Augmented Generation (RAG)
+Context-Aware AI Applications
+Memory-Aware AI Agents
+Document-to-Graph DB and Embeddings
+Multi-Modal LLM Applications
+Security and Guardrails
+AI Agent Orchestration with LangGraph
+Checkpointing in LangGraph
+Human-in-the-Loop Interruptions
+Tool Binding and API Calling
+Autonomous vs. Controlled Workflows
+MCP Servers
+Guardrails for AI Models
+Model Fine-Tuning
+LLM as a Judge Technique
+Deployment on AWS
+Cypher Query Context Retrieval
+
+
+##########################Tech Stack##############################################
+
+Programming Language
+
+JavaScript / TypeScript
+LLM Models
+
+OpenAI
+DeepSeek
+Claude
+Frameworks
+
+LangChain.js – A framework for building AI-powered applications
+LangGraph.js – A tool for structuring and managing AI agent workflows
+LangSmit – Enabling efficient AI development and execution
+Tracing & Monitoring
+
+Langfuse (Docker) – Self-hosted traces for AI applications
+Memory and Vector Stores
+
+Quadrant DB – A scalable, efficient vector store
+Vector Embedding Models – Enhancing AI understanding through embeddings
+Infrastructure
+
+MCP Server – Managing AI inference and computation
+Neo4j Graph DB – Graph-based AI knowledge storage
+AWS – Scalable cloud deployment for AI applications
+
+
+###################PROJECT#################################################
+AI Persona Maker – Build a chatbot that talks like Piyush Garg & Hitesh
+AI-Powered CLI Cursor – Create a CLI-based cursor that can vibe code for you
+Voice Powered AI Girlfriend – We are going to build a Voice-based AI Girlfriend for Piyush Garg
+Voice-Powered Vibe Coder – Vibe code just with your voice. Example: "Hey AI, Build a To-do App for me in React"
+AI-Powered Website Bot – Enabling AI-driven interactions with website content
+
+
